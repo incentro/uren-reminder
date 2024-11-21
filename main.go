3fb0700dcf19fffc61a527a3757629baa4720321
Moduo <tom.oud@incentro.com>
2024-11-21 14:54:13 +0100

    chore: split up code for getting message from file

--- main.go
+++ main.go
@@ -14,29 +14,39 @@ type MessageFile struct {
 	Messages []string `json:"messages"`
 }
 
-func main() {
-	token := os.Getenv("SLACK_TOKEN")
-	channelID := os.Getenv("SLACK_CHANNEL_ID")
-	afasUrl := os.Getenv("AFAS_URL")
-	client := slack.New(token)
-
-	jsonFile, err := os.ReadFile("input/messages.json")
+func getRandomMessageFromFile(fileName string) (string, error) {
+	jsonFile, err := os.ReadFile(fileName)
 	if err != nil {
 		fmt.Printf("Failed to read JSON file: %v\n", err)
-		return
+		return "", err
 	}
 
 	var file MessageFile
 	err = json.Unmarshal(jsonFile, &file)
 	if err != nil {
 		fmt.Printf("Failed to parse JSON: %v\n", err)
-		return
+		return "", err
 	}
 
 	randomIndex := rand.Intn(len(file.Messages))
-	selectedMessage := createBlock(file.Messages[randomIndex], afasUrl)
+	return file.Messages[randomIndex], nil
+}
+
+func main() {
+	token := os.Getenv("SLACK_TOKEN")
+	channelID := os.Getenv("SLACK_CHANNEL_ID")
+	afasUrl := os.Getenv("AFAS_URL")
+	client := slack.New(token)
+
+	msg, err := getRandomMessageFromFile("input/messages.json")
+	if err != nil {
+		fmt.Printf("Failed to parse JSON: %v\n", err)
+		return
+	}
+
+	blocks := createBlock(msg, afasUrl)
 	if shouldSendMessage(time.Now()) {
-		_, _, err := client.PostMessage(channelID, slack.MsgOptionBlocks(selectedMessage...))
+		_, _, err := client.PostMessage(channelID, slack.MsgOptionBlocks(blocks...))
 		if err != nil {
 			log.Fatal("Unable to send message to Slack", err)
 			return
